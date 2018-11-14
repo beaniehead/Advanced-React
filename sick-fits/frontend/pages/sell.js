825d822626b684963f7318233106ae03b8dfcf1d
Dicky <17915329+beaniehead@users.noreply.github.com>
2018-11-13 23:07:58 -0500

    Section 1 complete

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -2,11 +2,8 @@ import Link from 'next/link';
 
 const Sell = props => (
   <div>
-    <Link href="/index">
-      <a>Home</a>
-    </Link>
     <p>Sell!</p>
   </div>
-)
+);
 
 export default Sell;
