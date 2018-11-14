3fba7e49ed9d8e00650f3b0613a885828cf99bd2
Rich T <richard.todd@shopify.com>
2018-11-14 17:18:21 -0500

    Styled Comps

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -3,13 +3,23 @@ import styled from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
 const StyledPage = styled.div`
   background: white;
-  color: black;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
 `;
