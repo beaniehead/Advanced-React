58a218ce3b2676c2d47a1589cd9985cf145337df
Rich T <richard.todd@shopify.com>
2018-11-14 15:47:20 -0500

    Mac workspace

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -3,14 +3,25 @@ import styled from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
 class Page extends Component {
   render() {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
-        {this.props.children}
-      </div>
+        <Inner>{this.props.children}</Inner>
+      </StyledPage>
     );
   }
 }
