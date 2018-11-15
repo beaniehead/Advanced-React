0a0f9202ebe01315166960dc582401e162cc1990
Dicky <17915329+beaniehead@users.noreply.github.com>
2018-11-14 22:10:16 -0500

    Module 2 complete

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,18 @@
 import Link from 'next/link';
 import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Nav from './Nav';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -8,21 +20,44 @@ const Logo = styled.h1`
   position: relative;
   z-index: 2;
   transform: skew(-7deg);
+
   a {
     padding: 0.5rem 1rem;
-    background: ${props => props.theme.red};
+    background: ${props => props.theme.blue};
     color: white;
     text-transform: uppercase;
     text-decoration: none;
   }
-  @media (max-width: 1300px) {
+
+  @media (max-width: ${props => props.theme.$large}) {
     margin: 0;
     text-align: center;
   }
 `;
 
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+
+    @media (max-width: ${props => props.theme.$large}) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightGrey};
+  }
+`;
+
 const Header = () => (
-  <div>
+  <StyledHeader>
     <div className="bar">
       <Logo>
         <Link href="/">
@@ -35,7 +70,7 @@ const Header = () => (
       <p>Search</p>
       <div>Cart</div>
     </div>
-  </div>
+  </StyledHeader>
 );
 
 export default Header;
