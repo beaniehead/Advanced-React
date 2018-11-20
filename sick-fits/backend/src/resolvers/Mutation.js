6aa2a30414ee40d504081d1adc02a41d354e4bcc
Dicky <17915329+beaniehead@users.noreply.github.com>
2018-11-19 23:51:02 -0500

    Module 3 completed

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,25 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parents, args, ctx, info) {
+    // To Do - Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    return item;
+  },
+  // createDog(parents, args, ctx, info) {
+  //   // create a dog!
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // },
+};
 
 module.exports = Mutations;
